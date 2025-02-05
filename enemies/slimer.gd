08ebe1af437adab8ed7a3eac72d6331781264ab9
Louis Servant <louservant@gmail.com>
2025-02-04 20:19:11 -0600

    make slimer change directions and start moving only on camera

--- enemies/slimer.gd
+++ enemies/slimer.gd
@@ -7,26 +7,39 @@ extends Item
 @onready var wallray_collider = $ray_wallcheck
 @onready var sprite = $Sprite2D
 
+var is_moving = false
+
 func _ready() -> void:
   if direction == DIRECTION.LEFT:
-    speed = -speed
-    sprite.flip_h = true
+    change_direction()
 
 func _physics_process(delta: float) -> void:
-  if collider.disabled:
+  if collider.disabled or not is_moving:
     return
+
   # Add the gravity.
   if not is_on_floor():
     velocity += get_gravity() * delta
   
   if wallray_collider.is_colliding():
-    direction = (direction + 1) % 2
-    wallray_collider.target_position.x = -wallray_collider.target_position.x
-    speed *= -1
-    sprite.flip_h = !sprite.flip_h
+    print(str(name, "is colliding"))
+    change_direction()
     
   velocity.x = speed * 500 * delta
   move_and_slide()
+
+func start_moving():
+  is_moving = true
+
+func change_direction():
+  if direction == DIRECTION.LEFT:
+    direction = DIRECTION.RIGHT
+  else:
+    direction = DIRECTION.LEFT
+    
+  wallray_collider.target_position.x = -wallray_collider.target_position.x
+  speed *= -1
+  sprite.flip_h = !sprite.flip_h
 
 func die():
   queue_free()
@@ -45,3 +58,10 @@ func _on_collider_stomp_body_entered(body:Node2D) -> void:
   
   var player: Player = body
   die()
+
+
+func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
+  start_moving()
+  
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+  queue_free()
