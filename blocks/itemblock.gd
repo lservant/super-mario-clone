9edf68fab3c1417e88f3cdb250140795721d4990
Louis Servant <louservant@gmail.com>
2025-02-06 22:15:44 -0600

    add hidden and secret blocks

--- blocks/itemblock.gd
+++ blocks/itemblock.gd
@@ -2,16 +2,28 @@ extends StaticBody2D
 
 @onready var anim: AnimationPlayer = $AnimationPlayer
 @onready var collider: CollisionShape2D = $CollisionShape2D
+@onready var block: Sprite2D = $block
 
 @export_file() var item_path: String
 @export var coins: int = 1
+@export var is_hidden: bool = false
+@export var is_secret: bool = false
 
 var has_item
 var item_scene: PackedScene
 
+const EMPTY_FRAME = 8
+const BRICK_FRAME = 1
+
 func _ready() -> void:
+  block.visible = !is_hidden
   has_item = !item_path.is_empty()
   item_scene = load(item_path)
+
+  if is_secret:
+    block.frame = BRICK_FRAME
+  else:
+    anim.play("idle")
 
 func _on_underside_body_entered(body: Node2D) -> void:
   if body.name != "player":
@@ -19,14 +31,17 @@ func _on_underside_body_entered(body: Node2D) -> void:
   
   var player: Player = body
   
-  bounce()
-  player.change_state("fall")
+  if not is_empty():
+    bounce()
 
   if has_item:
     give_item()
   elif coins > 0:
     give_coin()
   
+  if is_empty():
+    block.frame = EMPTY_FRAME
+    
 func give_item():
   var item: Item = item_scene.instantiate()
   var item_tween = item.create_tween()
@@ -41,10 +56,10 @@ func give_item():
 func give_coin():
   anim.play("give_coin")
   coins -= 1
+  Game.add_coin()
 
 func bounce():
   anim.play("bounce")
 
-func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-  if anim_name == "bounce":
-    anim.play("RESET")
+func is_empty():
+  return not has_item and coins == 0
