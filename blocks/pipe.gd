aa06e63ee917a1de651b788202a1aa4ef1fe9375
Louis Servant <louservant@gmail.com>
2025-02-13 21:42:42 -0600

    only correct teleport

--- blocks/pipe.gd
+++ blocks/pipe.gd
@@ -7,11 +7,37 @@ class_name Pipe
 const minHeight = 2
 const spriteHeight = 16.0
 
+const ENTER_LEFT = -90.0
+const ENTER_RIGHT = 90.0
+const ENTER_TOP = 0.0
+const ENTER_BOTTOM = 180.0
+enum ENTER_DIR {
+  LEFT = 0,
+  RIGHT = 1,
+  TOP = 2,
+  BOTTOM = 3,
+}
+var enter_dir: ENTER_DIR = ENTER_DIR.TOP
+
 @onready var top: Sprite2D = $top
 @onready var bottom: Sprite2D = $bottom
 @onready var collider: StaticBody2D = $body
 
 func _ready() -> void:
+  var rounded_rotation = roundf(rotation_degrees)
+  if rounded_rotation == ENTER_LEFT or rounded_rotation == ENTER_BOTTOM:
+    top.flip_h = true
+    bottom.flip_h = true
+
+  if rounded_rotation == ENTER_TOP:
+    enter_dir = ENTER_DIR.TOP
+  elif rounded_rotation == ENTER_BOTTOM:
+    enter_dir = ENTER_DIR.BOTTOM
+  elif rounded_rotation == ENTER_LEFT:
+    enter_dir = ENTER_DIR.LEFT
+  elif rounded_rotation == ENTER_RIGHT:
+    enter_dir = ENTER_DIR.RIGHT
+
   if height < minHeight:
     height = minHeight
 
@@ -35,10 +61,13 @@ func _on_teleporter_body_entered(body: Node2D) -> void:
   var player: Player = body
   player.mount_pipe(self)
 
-func teleport_player(player: Player) -> void:
+func teleport_player(player: Player, dir: ENTER_DIR) -> bool:
+  if dir != enter_dir:
+    return false
   player.position = teleport_to.position
   player.is_on_pipe = false
   player.mounted_pipe = null
+  return true
 
 func _on_teleporter_body_exited(body: Node2D) -> void:
   if body.name != "player":
--- player/states/walk.gd
+++ player/states/walk.gd
@@ -8,7 +8,15 @@ func _physics_process(delta: float) -> void:
     return
   
   handle_crouch()
-  
+
+  if player.is_on_pipe and player.velocity.x == 0:
+    if Input.is_action_pressed("move_right"):
+      if player.mounted_pipe.teleport_player(player, Pipe.ENTER_DIR.LEFT):
+        return
+    elif Input.is_action_pressed("move_left"):
+      if player.mounted_pipe.teleport_player(player, Pipe.ENTER_DIR.RIGHT):
+        return
+
   if player.is_running:
     player.anim_movement.play("run")
   else:
